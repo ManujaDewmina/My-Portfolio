d383b7af56e1df3b9a213136a3e6a06d8d255ddf
Manuja Dewmina <92631934+ManujaDewmina@users.noreply.github.com>
2024-04-16 16:55:24 +0530

    Update PersonalData.js

--- src/Data/PersonalData.js
+++ src/Data/PersonalData.js
@@ -6,7 +6,7 @@ const PersonalData = {
     email:"manuja1bc@gmail.com",
     address:"Colombo, Sri Lanka",
     aboutMe:"I am a passionate computer engineering undergraduate who is interested in learning and exploring new things in the field of computer engineering. I consider my self hardworking in innovative technical backgrounds and also, I am a well responsible, excellent team player, and coordinator eager to learn and develop with a team. Interested in Cloud computing, Data management, ML and AI.",
-    resumeLink:'https://drive.google.com/file/d/1E5fk_2XQPDSaN-Vwkjtmt1_pVp4KVsAV/view?usp=sharing',
+    resumeLink:'https://drive.google.com/file/d/1YhPFoWnfY3m27CcXERXtR3LXobmbWWPE/view?usp=sharing',
 }
 export const autoTypeData=['Data Eng. enthusiast', 'ML and AI enthusiast','Backend Developer','Frontend Developer'];
 export default PersonalData;
