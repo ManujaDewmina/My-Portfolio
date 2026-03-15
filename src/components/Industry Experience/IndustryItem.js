a863e97c007f4a8af85fa2ae529a4c5066c1ff4b
ManujaDewmina <manuja1bc@gmail.com>
2026-03-15 22:00:18 +0530

    Restructure industry layout: positions timeline then description

--- src/components/Industry Experience/IndustryItem.js
+++ src/components/Industry Experience/IndustryItem.js
@@ -18,30 +18,26 @@ const IndustryItem = (props) => {
                 <div className={classes.industryInfo}>
                     <h3 style={{ color: uiColor }}>{data.StartYear} - {data.EndYear}</h3>
                     <h1>{data.companyName}</h1>
-                    {data.description && (
-                        <p className={classes.description} style={{color: nonThemeColor}}>{data.description}</p>
-                    )}
-                    {data.positions ? (
+                    {data.positions && (
                         data.positions.map((pos, idx) => (
                             <div key={idx} className={classes.positionBlock}>
                                 <h2 style={{ color: uiColor }}>{pos.title}</h2>
                                 <h3 style={{ color: uiColor }}>{pos.StartYear} - {pos.EndYear}</h3>
-                                <ul className={classes.details} style={{color: nonThemeColor}}>
-                                    {pos.details.map((item, index) =>
-                                        <li key={index}>{item}</li>
-                                    )}
-                                </ul>
                             </div>
                         ))
-                    ) : (
-                        <>
-                            <h2 style={{ color: uiColor }}>{data.Position}</h2>
-                            <ul className={classes.details} style={{color: nonThemeColor}}>
-                                {data.details.map((item, index) =>
-                                    <li key={index}>{item}</li>
-                                )}
-                            </ul>
-                        </>
+                    )}
+                    {!data.positions && data.Position && (
+                        <h2 style={{ color: uiColor }}>{data.Position}</h2>
+                    )}
+                    {data.description && (
+                        <p className={classes.description} style={{color: nonThemeColor}}>{data.description}</p>
+                    )}
+                    {data.details && (
+                        <ul className={classes.details} style={{color: nonThemeColor}}>
+                            {data.details.map((item, index) =>
+                                <li key={index}>{item}</li>
+                            )}
+                        </ul>
                     )}
                 </div>
             </div>
